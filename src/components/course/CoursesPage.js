0bedcd2365344293fbc44fccbebd30eb61e5fa2e
Aleksander Dudek <aleksander.dudek@yahoo.pl>
2018-12-02 18:48:51 +0100

    Redux added

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
+import * as courseActions from '../../actions/courseActions';
  class CoursesPage extends Component {
 
   constructor(props,context){
@@ -19,13 +22,23 @@ import React, { Component } from 'react';
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    // alert(`Saving ${this.state.course.title}`);
+    // this.props.dispatch(courseActions.createCourse(this.state.course));
+    //thanks to mapDispatchToProps
+    // this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
+
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add course</h2>
         <input
           type="text"
@@ -43,4 +56,39 @@ import React, { Component } from 'react';
   }
 }
 
-export default CoursesPage;
+//ownProps allow to get to props attached to this component
+function mapStateToProps(state, ownProps) {
+
+
+  //how I want to access ex. this.props.courses &&
+  //how I want to retrieve the data - state.courses
+  return {
+    courses: state.courses
+  };
+}
+
+//what actions I want to allow on props
+function mapDispatchToProps(dispatch) {
+//dispatch is injected by connect()
+  return {
+    // createCourse: (course) => dispatch(courseActions.createCourse(course))
+    actions: bindActionCreators(courseActions, dispatch)
+
+  };
+}
+
+
+//if mapDispatchToProps would be missing, we would still be
+//able to access it through this.props.dispatch
+
+//connect is HOC that injects dispatch-actions to props and maps
+//state (coming from )
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+
+CoursesPage.propTypes = {
+  //no longer exists on this component
+  // dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired,
+  // createCourse: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
+};
